a480fa64e30846c294db5af3a1e8e6f96a558f73
Nguyen Duc Tai <taind.25it@vku.udn.vn>
2026-06-04 13:00:34 +0700

    fix: OAuth problem

--- js/config.js
+++ js/config.js
@@ -1,3 +1,7 @@
-export const API_BASE_URL = "http://localhost:3643";
+export const API_BASE_URL = import.meta.env.PROD
+  ? "https://api.springwave.io.vn"
+  : "http://localhost:3643";
+
 export const CDN_DOMAIN = "https://cdn.springwave.io.vn";
-export const GOOGLE_CLIENT_ID = "438615062762-pmds797uuu9ufi19jvjnmkiach521p1p.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID =
+  "438615062762-pmds797uuu9ufi19jvjnmkiach521p1p.apps.googleusercontent.com";
