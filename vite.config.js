4518b22159eb893c05da5663acfe023333c33748
Nguyen Duc Tai <taind.25it@vku.udn.vn>
2026-06-04 12:43:20 +0700

    hfkljashgs

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'login.html'),
-        register: resolve(__dirname, 'register.html'),
-        explore: resolve(__dirname, 'explore.html'),
-        profile: resolve(__dirname, 'profile.html'),
-        hostActivity: resolve(__dirname, 'hostActivity.html'),
-        completeProfile: resolve(__dirname, 'complete-profile.html'),
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "login.html"),
+        register: resolve(__dirname, "register.html"),
+        explore: resolve(__dirname, "explore.html"),
+        profile: resolve(__dirname, "profile.html"),
+        hostActivity: resolve(__dirname, "hostActivity.html"),
+        completeProfile: resolve(__dirname, "complete-profile.html"),
       },
       output: {
-        assetFileNames: 'assets/[name][extname]'
-      }
+        assetFileNames: "assets/[name][extname]",
+      },
     },
   },
-})
+});
