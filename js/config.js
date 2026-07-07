e66a8cf366865304ed38edf7521349a6e861b84f
Nguyen Duc Tai <taind.25it@vku.udn.vn>
2026-07-07 23:30:02 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:3643";
+  import.meta.env.VITE_API_URL || "http://api.springwave.io.vn";
 export const CDN_DOMAIN =
   import.meta.env.VITE_CDN_DOMAIN || "https://cdn.springwave.io.vn";
 export const GOOGLE_CLIENT_ID =
